3c043d382210b848572fba4f02eab7d7001fc4d4
tom <zhangxuguang2007@126.com>
2016-05-26 09:35:58 +0800

    Delete some useless code

--- src/ts-model/src/main/java/com/tom/school/dao/sys/impl/SystemUserDaoImpl.java
+++ src/ts-model/src/main/java/com/tom/school/dao/sys/impl/SystemUserDaoImpl.java
@@ -10,32 +10,4 @@ public class SystemUserDaoImpl extends BaseDao<SystemUser> implements SystemUser
 		super(SystemUser.class);
 	}
 
-//	private SessionFactory sessionFactory;
-//
-//	public SessionFactory getSessionFactory() {
-//		return sessionFactory;
-//	}
-//
-//	public void setSessionFactory(SessionFactory sessionFactory) {
-//		this.sessionFactory = sessionFactory;
-//	}
-//
-//	public Session getSession() {
-//		return sessionFactory.getCurrentSession();
-//	}
-//
-//	@Resource(name = "sessionFactory")
-//	public void setSF(SessionFactory sessionFactory) {
-//		setSessionFactory(sessionFactory);
-//	}
-//
-//	@Override
-//	public SystemUser get(int id) {
-//		SystemUser user = new SystemUser();
-//		user.setId(1);
-//		user.setName("Tom");
-//		user.setPassword("Cogent01");
-//		return user;
-//	}
-
 }
