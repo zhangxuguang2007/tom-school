e121e7b775b5b5a69ae086a8e6973a56dc5f04d0
tom <zhangxuguang2007@126.com>
2016-06-12 18:28:20 +0800

    Add test.properties to config test context

--- src/ts-service/src/test/java/com/tom/school/test/TestContext.java
+++ src/ts-service/src/test/java/com/tom/school/test/TestContext.java
@@ -1,8 +1,23 @@
 package com.tom.school.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class TestContext {
 
-	public static String ServiceUrl = "http://localhost:8080/ts-service";
+	public static String ServiceUrl;
 	public static String Token;
+	
+	static{
+		try {
+			InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.properties");
+			Properties properties = new Properties();
+			properties.load(is);
+			ServiceUrl = properties.getProperty("server.url");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
