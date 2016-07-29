34703d553f43841a67c64325cf94f24ceef5789a
tom <zhangxuguang2007@126.com>
2016-07-29 11:37:53 +0800

    Add AuthorityFilter to check admin login

---
+++ src/ts-web/src/main/java/com/tom/school/web/filter/AuthorityFilter.java
@@ -0,0 +1,38 @@
+package com.tom.school.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import com.tom.school.core.entity.HttpRequestResult;
+import com.tom.school.core.utility.HttpRequestUtility;
+import com.tom.school.web.core.ServerContext;
+
+public class AuthorityFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpServerletRequest = (HttpServletRequest) request;
+		if(httpServerletRequest.getSession().getAttribute("token") == null){
+			String loginUrl = ServerContext.BaseRESTUrl + "authority/login?name=tom&password=Cogent01";
+			HttpRequestResult responseResult = HttpRequestUtility.doGet(loginUrl);
+			httpServerletRequest.getSession().setAttribute("token", new String(responseResult.getData()));
+		}
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
--- src/ts-web/src/main/java/com/tom/school/web/filter/AuthorityListener.java
+++
@@ -1,5 +0,0 @@
-package com.tom.school.web.filter;
-
-public class AuthorityListener {
-
-}
